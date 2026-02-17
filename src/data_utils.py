6a24bd1d70a89d45d48769a16cd0cf376f4b123a
DuHawke <duongduong190906@gmail.com>
2026-02-17 13:54:44 +0000

    data

--- src/data_utils.py
+++ src/data_utils.py
@@ -10,21 +10,27 @@ from typing import Dict, List, Any
 
 
 class TranslationDataset(Dataset):
-    """Wrapper cho Hugging Face dataset"""
-    
-    def __init__(self, hf_dataset):
-        self.dataset = hf_dataset
-    
+    def __init__(self, hf_dataset, source_lang='en', target_lang='vi'):
+        self.dataset    = hf_dataset
+        self.source_lang = source_lang
+        self.target_lang = target_lang
+
     def __len__(self):
         return len(self.dataset)
-    
+
     def __getitem__(self, idx):
         item = self.dataset[idx]
-        # opus-100 dùng key 'translation' thay vì 'source'/'target'
-        return {
-            'source': item['translation']['en'],
-            'target': item['translation']['vi'],
-        }
+        # Hỗ trợ cả 2 format
+        if 'translation' in item:
+            return {
+                'source': item['translation'][self.source_lang],
+                'target': item['translation'][self.target_lang],
+            }
+        else:
+            return {
+                'source': item['source'],
+                'target': item['target'],
+            }
 
 
 @dataclass
@@ -43,20 +49,28 @@ class NERTranslationDataCollator:
         }
 
 
-def filter_by_length(example, min_len=3, max_len=150):
-    """Lọc câu quá ngắn hoặc quá dài"""
-    src_words = len(example['source'].split())
-    tgt_words = len(example['target'].split())
+def filter_by_length(example, min_len=3, max_len=150, 
+                     source_lang='en', target_lang='vi'):
+    """Lọc câu quá ngắn hoặc quá dài — hỗ trợ format translation"""
+    # Lấy text theo đúng format của dataset
+    if 'translation' in example:
+        src_text = example['translation'][source_lang]
+        tgt_text = example['translation'][target_lang]
+    else:
+        src_text = example['source']
+        tgt_text = example['target']
+
+    src_words = len(src_text.split())
+    tgt_words = len(tgt_text.split())
     return (min_len <= src_words <= max_len) and (min_len <= tgt_words <= max_len)
 
 
-def prepare_dataset(dataset, min_len=3, max_len=150):
+def prepare_dataset(dataset, min_len=3, max_len=150,
+                    source_lang='en', target_lang='vi'):
     """Chuẩn bị dataset với filtering"""
-    dataset['train'] = dataset['train'].filter(
-        lambda x: filter_by_length(x, min_len, max_len)
+    filter_fn = lambda x: filter_by_length(
+        x, min_len, max_len, source_lang, target_lang
     )
-    dataset['validation'] = dataset['validation'].filter(
-        lambda x: filter_by_length(x, min_len, max_len)
-    )
-    
+    dataset['train']      = dataset['train'].filter(filter_fn)
+    dataset['validation'] = dataset['validation'].filter(filter_fn)
     return dataset
